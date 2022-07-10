99451c0b8addc7b5da8f427655aa042ce9672270
Madi <mads890@yahoo.com>
2022-07-09 18:51:40 -0700

    added functions

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function changeColor() {
     document.getElementById('crazy').style.color = "magenta";
 }
+
+function printText(text) {
+    console.log(text);
+}
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+printText('howdy!');
+add(2, 5);
